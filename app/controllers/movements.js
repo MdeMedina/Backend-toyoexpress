ccafb668d3b52aacabde2315427b437fdbfad144
MdeMedina <mamedina770@gmail.com>
2023-03-21 19:41:31 -0300

    Config

--- app/controllers/movements.js
+++ app/controllers/movements.js
@@ -1,13 +1,13 @@
 const { Ingreso, Egreso, Movimiento } = require("../models/movimiento");
 const { DateTime } = require("luxon");
-const { formatDateHoy} = require("../helpers/dates/dates");
+const { formatDateHoy } = require("../helpers/dates/dates");
 
 const crearMovimiento = async (req, res) => {
   const { body } = req;
   const hoy = `${formatDateHoy(new Date())}`;
   const name = body.name;
   const isMove = await Movimiento.find({});
- 
+
   let identificador;
   if (isMove.length < 9) {
     let par = isMove.length + 1;
@@ -23,7 +23,14 @@ const crearMovimiento = async (req, res) => {
   const concepto = body.concepto;
   const monto = body.monto;
   const fecha = body.fecha;
+  let aFecha = "";
   const cuenta = body.cuenta;
+  let vale = "";
+  console.log(cuenta);
+  if (cuenta == "CajaChica") {
+    vale = identificador;
+    aFecha = fecha;
+  }
   const move = await Movimiento.create({
     identificador: identificador,
     cuenta: cuenta,
@@ -37,11 +44,11 @@ const crearMovimiento = async (req, res) => {
     monto: monto,
     fecha: fecha,
     name: name,
-    afecha: "",
+    afecha: aFecha,
     email: body.email,
-    vale: "",
+    vale: vale,
     messageId: body.messageId,
-    disabled: false
+    disabled: false,
   });
   const movimientos = await Movimiento.find({});
 
@@ -49,87 +56,86 @@ const crearMovimiento = async (req, res) => {
   movimientos.map((n) => {
     moves.push(n);
   });
-  res.status(201).send({message: "Movimiento creado con exito", moves, status:200});
-}
+  res
+    .status(201)
+    .send({ message: "Movimiento creado con exito", moves, status: 200 });
+};
 
 const deleteMoves = async (req, res) => {
   const { body } = req;
   const filter = body.identificador;
-    const move = await Movimiento.findOneAndUpdate(
-      { identificador: filter },
-      {
-        disabled: true
-      },
-      { new: true }
-    );    
-    const movimientos = await Movimiento.find({});
-    let moves = [];
-    movimientos.map((n) => {
-      moves.push(n);
-    });
-    res.status(200).send({moves, status:200});
-  
+  const move = await Movimiento.findOneAndUpdate(
+    { identificador: filter },
+    {
+      disabled: true,
+    },
+    { new: true }
+  );
+  const movimientos = await Movimiento.find({});
+  let moves = [];
+  movimientos.map((n) => {
+    moves.push(n);
+  });
+  res.status(200).send({ moves, status: 200 });
 };
-
 
 const modificarMovimiento = async (req, res) => {
   const { body } = req;
   const filter = body.identificador;
-  let identificador = body.identificador
-  let id = body.id
-  identificador = `${id}-${identificador[2]}${identificador[3]}${identificador[4]}`
+  let identificador = body.identificador;
+  let id = body.id;
+  identificador = `${id}-${identificador[2]}${identificador[3]}${identificador[4]}`;
   console.log({
     efectivo: body.efectivo,
     zelle: body.zelle,
     bs: body.bs,
     change: body.change,
     dollars: body.dollars,
-  })
+  });
   const concepto = body.concepto;
   const monto = body.monto;
   const cuenta = body.cuenta;
   const fecha = body.fecha;
-    const move = await Movimiento.findOneAndUpdate(
-      { identificador: filter },
-      {
-        identificador,
-        cuenta: cuenta,
-        concepto: concepto,
-        efectivo: body.efectivo,
-        zelle: body.zelle,
-        bs: body.bs,
-        change: body.change,
-        dollars: body.dollars,
-        otro: body.otro,
-        monto: monto,
-        fecha: fecha,
-      },
-      { new: true }
-    );
-    const movimientos = await Movimiento.find({});
-    let moves = [];
-    movimientos.map((n) => {
-      moves.push(n);
-    });
-    res.status(200).send({moves, status:200});
+  const move = await Movimiento.findOneAndUpdate(
+    { identificador: filter },
+    {
+      identificador,
+      cuenta: cuenta,
+      concepto: concepto,
+      efectivo: body.efectivo,
+      zelle: body.zelle,
+      bs: body.bs,
+      change: body.change,
+      dollars: body.dollars,
+      otro: body.otro,
+      monto: monto,
+      fecha: fecha,
+    },
+    { new: true }
+  );
+  const movimientos = await Movimiento.find({});
+  let moves = [];
+  movimientos.map((n) => {
+    moves.push(n);
+  });
+  res.status(200).send({ moves, status: 200 });
 };
 
 const modificarStatus = async (req, res) => {
-
   const { body } = req;
   const filter = body.identificador;
 
-    const isVale = await Movimiento.findOne({ vale: body.vale });
-    if (isVale) {
-      res.status(403).send("Este numero de aprobacion ya existe");
-    } else {
-      const move = await Movimiento.findOneAndUpdate(
-        { identificador: filter },
-        { vale: body.vale, aFecha: body.aFecha },
-        { new: true }
-      );
-      res.status(200).send(move);
-    }
+  const isVale = await Movimiento.findOne({ vale: body.vale });
+  if (isVale) {
+    res.status(403).send("Este numero de aprobacion ya existe");
+  } else {
+    const move = await Movimiento.findOneAndUpdate(
+      { identificador: filter },
+      { vale: body.vale, aFecha: body.aFecha },
+      { new: true }
+    );
+    res.status(200).send(move);
+  }
 };
 
 const getMoves = async (req, res) => {
@@ -141,7 +147,7 @@ const getMoves = async (req, res) => {
   res.status(200).send(moves);
 };
 module.exports = {
- crearMovimiento,
+  crearMovimiento,
   getMoves,
   modificarStatus,
   deleteMoves,
