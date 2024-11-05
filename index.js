5ec9723c497447df146eaf8236e9f7af05240c6e
MdeMedina <70539248+MdeMedina@users.noreply.github.com>
2024-11-04 23:37:47 -0300

    Prueba de cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let server = http.createServer(app);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cors({
-  origin: 'http://front.toyoxpress.com/', // Reemplaza con el origen de tu frontend
+  origin: 'http://front.toyoxpress.com', // Reemplaza con el origen de tu frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -52,7 +52,7 @@ app.use(express.static("app"));
 
 let io = new Server(server, {
   cors: {
-    origin: "http://front.toyoxpress.com/",
+    origin: "http://front.toyoxpress.com",
     methods: ["GET", "POST", "UPDATE"],
   },
 });
