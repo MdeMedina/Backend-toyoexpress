b970bdfca3460509beddf40acb6a21901e4d498a
MdeMedina <70539248+MdeMedina@users.noreply.github.com>
2024-11-06 09:37:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/pdf", require("./app/routes/dataPDF"));
 app.use(express.static("app"));
 
 app.get('/events', (req, res) => {
-  console.log("llegue a events")
+  console.log("llegue a events index.js")
   addClient(res);
 });
 
