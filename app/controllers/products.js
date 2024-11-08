48b2aa2cc31e15d902fa6243a47f8678e4965406
MdeMedina <70539248+MdeMedina@users.noreply.github.com>
2024-11-08 20:52:23 -0300

    productos

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -77,7 +77,7 @@ const makeProducts = async (req, res) => {
  Producto.insertMany(arrayBueno);
 
   const params = {
-    QueueUrl: "https://sqs.us-east-2.amazonaws.com/872515257475/ToyoxpressDev.fifo",
+    QueueUrl: "https://sqs.us-east-2.amazonaws.com/872515257475/Toyoxpress.fifo",
     MessageBody: JSON.stringify({arr: arrayChunked[0], index: 0, maximo: length}),
     MessageGroupId: "grupo-1",
     MessageDeduplicationId: `0`, 
@@ -144,7 +144,7 @@ console.log(data);
 
   // Parámetros para eliminar el mensaje en SQS
  const deleteParams = {
-    QueueUrl: 'https://sqs.us-east-2.amazonaws.com/872515257475/ToyoxpressDev.fifo',
+    QueueUrl: 'https://sqs.us-east-2.amazonaws.com/872515257475/Toyoxpress.fifo',
     ReceiptHandle: body.receiptHandle
   };
 
@@ -158,7 +158,7 @@ global.shared.sendToClients(JSON.stringify({ index: body.index+1, maximo: body.m
 
 if (arrayChunked.length > body.index + 1 ) {
   const params = {
-    QueueUrl: "https://sqs.us-east-2.amazonaws.com/872515257475/ToyoxpressDev.fifo",
+    QueueUrl: "https://sqs.us-east-2.amazonaws.com/872515257475/Toyoxpress.fifo",
     MessageBody: JSON.stringify({arr: arrayChunked[body.index+1], index: body.index+1, maximo: body.maximo}),
     MessageGroupId: "grupo-1",
     MessageDeduplicationId: `${body.index+1}`, 
